5c87aa3d549b4099509814681841b6fdcad7be4b
Fady Hatta <Fadynhatta@gmail.com>
2024-03-05 15:43:53 +0100

    Added stuff

--- jsonDemoNet23/src/pages/Projects.jsx
+++ jsonDemoNet23/src/pages/Projects.jsx
@@ -3,10 +3,12 @@ import MyProjects from "../assets/data.json"
 export default function Projects(){
     return (
         <section>
+
+            
                 <h2>Made by: {MyProjects.user}</h2>
                 { MyProjects.projects.map(proj => {
                         return (
-                            <article>
+                            <article key={proj.name}>
                                 <h3>Project name: {proj.name}</h3>
                                 <p>{proj.description}</p>
                             </article>
